8b0089b3610f247bb479a3c17b24c42f065ca88b
basarane <ersin@komputer.com.tr>
2023-02-15 15:57:21 +0300

    git merge/push

--- server/services/export/save-entity-json.js
+++ server/services/export/save-entity-json.js
@@ -18,7 +18,26 @@ const commitEntityJson = async ({ }) => {
     // const res = child_process.execSync(`node strapi-scripts/export-data.js`, { stdio: 'inherit' });
     const res1 = child_process.execSync(`git add Entity/*`, { stdio: 'inherit' });
     const res2 = child_process.execSync(`git commit -m "Commit from strapi"`, { stdio: 'inherit' });
-    return { success: true, res: "Add Res: " + res1 + " Commit Res:" + res2 };
+    const currentBranch = child_process.execSync('git branch --show-current').toString().trim();
+    let res3, res4, res5;
+    const toBranch = "staging";
+    if (toBranch != currentBranch) {
+        res3 = child_process.execSync(`git checkout ${toBranch}`, { stdio: 'inherit' });
+        res4 = child_process.execSync(`git merge ${currentBranch}`, { stdio: 'inherit' });
+        res5 = child_process.execSync(`git push origin ${toBranch}`, { stdio: 'inherit' });
+        res7 = child_process.execSync(`git checkout ${currentBranch}`, { stdio: 'inherit' });
+        res6 = child_process.execSync(`git push origin ${currentBranch}`, { stdio: 'inherit' });
+    }
+    return {
+        success: true,
+        res: " Add Res: " + res1 +
+            " Commit Res:" + res2 +
+            " Checkout to Res:" + res3 +
+            " Merge to current:" + res4 +
+            " push to Res:" + res5 +
+            " checkout current Res:" + res6 +
+            " push current Res:" + res7;
+    };
 }
 
 module.exports = {
