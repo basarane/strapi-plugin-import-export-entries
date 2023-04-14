6da881155523fdbb32fb05cb9f5c1ad5f9dc38d2
basarane <ersin@komputer.com.tr>
2023-04-14 18:58:43 +0300

    Add stage checkbox to new/edit collection
    When checked it gives find and findOne permissions to the collection. It also updates sotka-config.js so that the collection is used during import/export.

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,5 +1,49 @@
 'use strict';
 
-module.exports = ({ strapi }) => {
+
+const registerModelsHooks = async () => {
+  const fs = require("fs");
+  const stageModels = Object.values(strapi.contentTypes)
+    .filter((contentType) => contentType.pluginOptions && contentType.pluginOptions['import-export'] && contentType.pluginOptions['import-export'].stage)
+    .map((contentType) => contentType.uid);
+
+  const roles = await strapi
+    .service("plugin::users-permissions.role")
+    .find();
+  const _public = await strapi
+    .service("plugin::users-permissions.role")
+    .findOne(roles.filter((role) => role.type === "public")[0].id);
+
+  for (const permission of Object.keys(_public.permissions)) {
+    for (const controller of Object.keys(
+      _public.permissions[permission].controllers
+    )) {
+      if (permission.startsWith("api") && stageModels.indexOf(`${permission}.${controller}`) >= 0) {
+        console.log("ALLOW PUBLIC ACCESS", permission, controller);
+        const perms = _public.permissions[permission].controllers[
+          controller
+        ];
+        perms.find.enabled = true;
+        if (perms.findOne)
+          perms.findOne.enabled = true;
+      }
+    }
+  }
+  await strapi
+    .service("plugin::users-permissions.role")
+    .updateRole(_public.id, _public);
+
+  const origSotkaConfig = fs.readFileSync("sotka-config.js", "utf8");
+  const sotkaConfig = origSotkaConfig.replace(/stageCollections\s*:\s*"[^"]*"/, `stageCollections: "${stageModels.join(",")}"`);
+  if (origSotkaConfig !== sotkaConfig) {
+    fs.writeFileSync("sotka-config.js", sotkaConfig);
+  }
+  console.log(sotkaConfig);
+
+};
+module.exports = async ({ strapi }) => {
   // bootstrap phase
+  console.log("BOOTSTRAP");
+  await registerModelsHooks();
+  console.log("REGISTERED HOOKS");
 };
